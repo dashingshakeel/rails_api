a19fce637a6a6610a97178d70004db4c9f3accfc
dashingshakeel <shkeelrauf@gmail.com>
2017-01-23 15:59:25 +0500

    Added email parameter and find user by email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action  only: [:show, :update, :destroy]
 
   # GET /users
   def index
     @users = User.all
 
-    render json: @users
+    render json: @usersra
   end
 
   # GET /users/1
   def show
-    render json: @user
+    user = User.find_by(email: params[:email])
+    render :json => user.as_json 
   end
 
   # POST /users
@@ -40,9 +41,7 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+
 
     # Only allow a trusted parameter "white list" through.
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  scope '/api' do 
-    scope '/users' do 
-      put '/' => 'users#new'
-      post '/' => 'users#create'
-    end
-    get 'emails' => 'users#index'
+ 
+  scope '/api' do
+    resources :users, only: [:show, :index,:create] , param: :email
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

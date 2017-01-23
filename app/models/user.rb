8e89cd40cf91a46f155389bed85c044932c7c9f3
dashingshakeel <shkeelrauf@gmail.com>
2017-01-23 16:13:22 +0500

    Validate model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   require 'digest/sha1'
   before_save {self.password = Digest::SHA1.hexdigest(password)}
-  
+  before_save { email.downcase! }
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence:   true, length: { maximum: 255 },
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 end
